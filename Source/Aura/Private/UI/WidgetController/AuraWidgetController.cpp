3d2d17a87c36f0b6c84d65d5cc521dc8cac95d1a
Gerhard Meyer <gerhard.meyer@pragmaworld.net>
2025-06-28 21:48:50 +0200

    hud part 1

--- Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -1,4 +1,4 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "UI/WidgetController/AuroWidgetController.h"
+#include "UI/WidgetController/AuraWidgetController.h"
